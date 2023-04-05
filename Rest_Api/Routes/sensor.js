5b549e646929a77b8e5b5c993718ebf15b517f6d
rami mustonen <t0mura00@students.oamk.fi>
2023-04-05 12:50:42 +0300

    hakuja muutettu ja varausta korjailtu

--- Rest_Api/Routes/sensor.js
+++ Rest_Api/Routes/sensor.js
@@ -13,7 +13,13 @@ router.get('/test',function(request,response){
         }
     })
 })
-router.get('/test2/:id?',function(request,response){
+//network test
+router.get('/hello',function(req,res){
+    console.log('hello')
+    res.send('hello')
+})
+//Get parking slot statuses of parking garage
+router.get('/:id?',function(request,response){
     parkit.getbyid(request.params.id,function(err,result){
         if(err){
             response.json(err)
@@ -33,6 +39,75 @@ router.get('/test2/:id?',function(request,response){
         }
     })
 })
+//Reserve parking slot
+router.put('/',function(request,response){
+    parkit.getSlot(request.body.id,function(err,res){
+        if(err){
+            console.log('error happened')
+        }else{
+            if(res.length!=0){
+            if(res[0].etaisyys<500 || res[0].varattu==1){
+                console.log('Paikka käytössä tai varattu')
+                response.status(404).send('Already in use')
+            }else{
+                parkit.addvaraus(request.body.id,function(err,result){
+                    if(err){
+                        response.json(err)
+                    }else{
+                        console.log(res[0].idParkit)
+                        resv.push({id:res[0].idParkit,count:0})
+                        response.send('succesfull')
+                    }
+                })
+            }
+        }else{
+            console.log('no such slot')
+            response.status(404).send('no such slot')
+        }
+        }
+    })
+})
+
+//Free parking reservations when reserved space in use
+function checkReservation(){
+    parkit.check(function(err,result){
+        if(err){
+            console.log('Error in checking reservations')
+        }else{
+            var arr = JSON.parse(JSON.stringify(result))
+            arr.forEach(e => {
+                if(e.varattu==1 && e.etaisyys<500){
+                    parkit.freevaraus(e.idParkit,function(err,res){
+                        if(err){
+                            console.log('Could not change park '+e.idParkit)
+                        }else{
+                            console.log("Park with id"+e.idParkit+" reservation freed")
+                        }
+                    })
+                }
+            });
+        }
+    })
+    if(resv.length>0){
+    resv.forEach(element => {
+        if(element.count>2){
+            parkit.freevaraus(element.id,function(err,res){
+                if(err){
+                    console.log('Could not change park '+element.id)
+                }else{
+                    resv.splice(resv.indexOf(element.id),1)
+                    console.log(resv)
+                    console.log("Park with id"+element.id+" reservation freed")
+                }
+            })
+        }else{
+            element.count+=1
+        }
+    });
+}
+}
 
 
+const checkTime = 1000*60*1
+setInterval(checkReservation, checkTime)
 module.exports = router;
--- Rest_Api/model/parkit.js
+++ Rest_Api/model/parkit.js
@@ -5,7 +5,11 @@ const parkit={
         return db.query('select count(varattu),sijainti from ParkkiTalo join Parkit on ParkkiTalo.id=Parkit.ParkkiTalo_id where varattu=0 group by sijainti',callback)
     },
     getbyid:function(id,callback){
-        return db.query('select *from Parkit where ParkkiTalo_id=?',[id],callback)
+        return db.query('select idParkit,etaisyys,varattu from Parkit join ParkkiTalo on Parkit.ParkkiTalo_id=ParkkiTalo.id where sijainti=?',[id],callback)
+    },
+    //Get parking slot info with id
+    getSlot:function(id,callback){
+        return db.query('select * from Parkit where idParkit=?',[id],callback)
     },
     addvaraus:function(id,name,callback){
         return db.query('update Parkit set varattu = 1 where idParkit=? and ParkkiTalo_id=?',[id,name],callback)
