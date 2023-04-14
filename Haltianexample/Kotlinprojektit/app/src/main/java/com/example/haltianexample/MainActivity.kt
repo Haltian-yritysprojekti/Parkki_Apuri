a049257d251d128181953d986d525cf1dc1662d6
elielll <eliel.latvala@gmail.com>
2023-04-14 10:06:38 +0300

    Update

--- Haltianexample/Kotlinprojektit/app/src/main/java/com/example/haltianexample/MainActivity.kt
+++ Haltianexample/Kotlinprojektit/app/src/main/java/com/example/haltianexample/MainActivity.kt
@@ -9,10 +9,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONArray
 import java.security.SecureRandom
 import java.security.cert.X509Certificate
 import javax.net.ssl.HostnameVerifier
@@ -24,112 +22,127 @@ import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
     var laskuri = true
-    //private lateinit var dataTextView: TextView
-    private val url = "https://ec2-13-49-138-78.eu-north-1.compute.amazonaws.com:3000/"
+    private var url = "https://ec2-13-49-138-78.eu-north-1.compute.amazonaws.com:3000/"
+    //private var url2 = "https://ec2-13-49-138-78.eu-north-1.compute.amazonaws.com:3000/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(supportActionBar != null){
+        if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
-        val myButtonA:Button = findViewById(R.id.bu_sijaintiA)
-        val myButtonB:Button = findViewById(R.id.bu_sijaintiB)
-        val myButtonC:Button = findViewById(R.id.bu_sijaintiC)
-        val textView1:TextView=findViewById(R.id.tv_sA)
-        val textView2:TextView=findViewById(R.id.tv_free_A)
-        val textView3:TextView=findViewById(R.id.tv_sB)
-        val textView4:TextView=findViewById(R.id.tv_free_B)
-        val textView5:TextView=findViewById(R.id.tv_sC)
-        val textView6:TextView=findViewById(R.id.tv_free_C)
-        val dataTextView1:TextView=findViewById(R.id.dataa)
-        val textViews = mutableListOf<TextView>(textView1,textView3,textView5)
-        val textViews1 = mutableListOf<TextView>(textView2,textView4,textView6)
-        //dataTextView = findViewById(R.id.dataa)
+        val myButtonA: Button = findViewById(R.id.bu_sijaintiA)
+        val myButtonB: Button = findViewById(R.id.bu_sijaintiB)
+        val myButtonC: Button = findViewById(R.id.bu_sijaintiC)
+        val textView1: TextView = findViewById(R.id.tv_sA)
+        val textView2: TextView = findViewById(R.id.tv_free_A)
+        val textView3: TextView = findViewById(R.id.tv_sB)
+        val textView4: TextView = findViewById(R.id.tv_free_B)
+        val textView5: TextView = findViewById(R.id.tv_sC)
+        val textView6: TextView = findViewById(R.id.tv_free_C)
+        val dataTextView1: TextView = findViewById(R.id.dataa)
+        val textViews = mutableListOf<TextView>(textView1, textView3, textView5)
+        val textViews1 = mutableListOf<TextView>(textView2, textView4, textView6)
 
-        myButtonA.setOnClickListener{
+        //makeJsonRequest(textViews1, textViews)
+        makeJsonRequest(dataTextView1,textViews1, textViews)
+
+
+        myButtonA.setOnClickListener {
             laskuri = !laskuri
             if (!laskuri) {
-                val myImageViewA:ImageView = findViewById(R.id.ivParkA)
+                val myImageViewC: ImageView = findViewById(R.id.ivParkC)
+                val myImageViewB: ImageView = findViewById(R.id.ivParkB)
+                val myImageViewA: ImageView = findViewById(R.id.ivParkA)
                 myImageViewA.visibility = View.VISIBLE
-            }
-            else{
-                val myImageViewA:ImageView = findViewById(R.id.ivParkA)
+                myImageViewB.visibility = View.GONE
+                myImageViewC.visibility = View.GONE
+                url = url + "sijainti%20A"
+                //makeJsonRequest(dataTextView1,textViews, textViews1)
+                //makeJsonRequest(textViews, textViews1)
+            } else {
+                val myImageViewA: ImageView = findViewById(R.id.ivParkA)
                 myImageViewA.visibility = View.GONE
             }
 
         }
-        myButtonB.setOnClickListener{
+        myButtonB.setOnClickListener {
             laskuri = !laskuri
-            if(!laskuri){
-                val myImageViewB:ImageView = findViewById(R.id.ivParkB)
+            if (!laskuri) {
+                val myImageViewB: ImageView = findViewById(R.id.ivParkB)
                 myImageViewB.visibility = View.VISIBLE
-            }
-            else{
-                val myImageViewB:ImageView = findViewById(R.id.ivParkB)
+            } else {
+                val myImageViewB: ImageView = findViewById(R.id.ivParkB)
                 myImageViewB.visibility = View.GONE
             }
         }
-        myButtonC.setOnClickListener{
+        myButtonC.setOnClickListener {
             laskuri = !laskuri
-            if(!laskuri){
-                val myImageViewC:ImageView = findViewById(R.id.ivParkC)
+            if (!laskuri) {
+                val myImageViewC: ImageView = findViewById(R.id.ivParkC)
                 myImageViewC.visibility = View.VISIBLE
-            }
-            else{
-                val myImageViewC:ImageView = findViewById(R.id.ivParkC)
+            } else {
+                val myImageViewC: ImageView = findViewById(R.id.ivParkC)
                 myImageViewC.visibility = View.GONE
             }
         }
 
-
-        // Create a trust manager that does not validate certificate chains
-        val trustAllCerts: Array<TrustManager> = arrayOf(object : X509TrustManager {
-            override fun getAcceptedIssuers(): Array<X509Certificate> {
-                return arrayOf()
-            }
-
-            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
-            }
-
-            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
-            }
-        })
-
-        // Install the all-trusting trust manager
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, trustAllCerts, SecureRandom())
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.socketFactory)
-
-        // Create an ssl socket factory with our all-trusting manager
-        val hostnameVerifier = HostnameVerifier { _, _ -> true }
-        HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier)
-
-        val request = JsonArrayRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                try {
-                    for(i in 0 until response.length()){
-                        val jsonObject = response.getJSONObject(i)
-                        textViews[i].text = jsonObject.getString("sijainti")
-                        textViews1[i].text = jsonObject.getString("vapaa")
-                        }
-                } catch (e: Exception) {
-                    Toast.makeText(this, "Error parsing JSON", Toast.LENGTH_SHORT).show()
-                }
-            },
-            { error ->
-                Toast.makeText(this, "Error retrieving data:${error.message}", Toast.LENGTH_LONG).show()
-                Log.e("Error retrieving data", error.toString())
-
-            }
-        )
-        Volley.newRequestQueue(this).add(request)
-
     }
+private fun makeJsonRequest(dataTextView1:TextView,textViews1:MutableList<TextView>,textViews:MutableList<TextView>) {
 
 
+    // Create a trust manager that does not validate certificate chains
+    val trustAllCerts: Array<TrustManager> = arrayOf(object : X509TrustManager {
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+    })
+
+    // Install the all-trusting trust manager
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, SecureRandom())
+    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.socketFactory)
+
+    // Create an ssl socket factory with our all-trusting manager
+    val hostnameVerifier = HostnameVerifier { _, _ -> true }
+    HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier)
+
+
+    val request = JsonArrayRequest(
+        Request.Method.GET, url, null,
+        { response ->
+            try {
+
+                for (i in 0 until response.length()) {
+                    val jsonObject = response.getJSONObject(i)
+                    textViews[i].text = jsonObject.getString("sijainti")
+                    textViews1[i].text = jsonObject.getString("vapaa")
+                }
+
+                Log.i("Update URL", url)
+                val jsonObject = response.getJSONObject(0)
+                dataTextView1.text=jsonObject.getString("idParkit")
+
+
+            } catch (e: Exception) {
+                Toast.makeText(this, "Error parsing JSON", Toast.LENGTH_SHORT).show()
+            }
+        },
+        { error ->
+            Toast.makeText(this, "Error retrieving data:${error.message}", Toast.LENGTH_LONG)
+                .show()
+            Log.e("Error retrieving data", error.toString())
+
+        }
+    )
+    Volley.newRequestQueue(this).add(request)
 
 }
 
 
-
+}
